c81c42e1549fa140e53ef809578f6c5ce9aa38f1
Antony Pyatkov <tltary@yandex.ru>
2019-05-16 14:05:41 +0400

    remove code in page, add script file

---
+++ rcl.js
@@ -0,0 +1,18 @@
+let link = document.querySelectorAll('a');
+let url  = window.location.href;
+for (let i = 0;i < link.length;i = i + 1) {
+	if (url == link[i].href) {
+		replaceTag(link[i]);
+	}
+}
+function replaceTag(element)
+{
+    let elementNew = document.createElement('span');
+    elementNew.innerHTML = element.innerHTML;
+    Array.prototype.forEach.call(element.attributes, function(attr) {
+        elementNew.setAttribute(attr.name, attr.value);
+    });
+    element.parentNode.insertBefore(elementNew, element);
+    element.parentNode.removeChild(element);
+    return elementNew;
+}
